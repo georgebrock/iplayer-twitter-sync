b9025b30f8cc4d8e95f572f7a1833685b3d898ad
Tom Lea <commit@tomlea.co.uk>
2010-06-12 22:21:10 +0100

    13 days and not enough sleep.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,20 @@
 class TweetsController < ApplicationController
   before_filter :login_required
   def index
-    range = Time.parse(params[:start])...Time.parse(params[:end])
-    p range.to_s
+    start_time = Time.parse(params[:start])
+    if params[:end]
+      end_time = Time.parse(params[:end])
+    else
+      end_time = start_time + params[:duration].to_i.minutes
+    end
+
+    range = start_time...end_time
+
     set = []
     Tweenumerator.new(current_user.twitter).each do |tweet|
       time = Time.parse(tweet["created_at"])
       break if time < range.first
-      if range.include?(time)
-        p time, range
-        set << tweet
-      end
+      set << tweet if range.include?(time)
     end
     render :json => set, :callback => params[:callback]
   end
