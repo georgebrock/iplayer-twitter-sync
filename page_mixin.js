8c161c075b6aaab4e7b07e6b8822366c54138061
George Brocklehurst <george.brocklehurst@gmail.com>
2010-06-12 23:24:02 +0100

    Bring on the tweets!

--- page_mixin.js
+++ page_mixin.js
@@ -1,4 +1,6 @@
 (function($) {
+  var twitterHost = "http://iplayer-sync.heroku.com";
+
   var authToken = function() {
     var token, matches;
 
@@ -13,8 +15,16 @@
 
     return token;
   }
+
+  var displayTweet = function(data) {
+    var img = "<img src=\"" + data.user.profile_image_url.replace(/_normal\./, '_mini.') + "\" alt=\"\" />";
+    var userLink = "<a href=\"http://www.twitter.com/" + data.user.screen_name + "\">" + data.user.screen_name + "</a>"
+    $tweets.prepend("<li>" + img + userLink + " " + data.text + "</li>");
+    $time.html(data.created_at);
+  }
+
   var $time = $("<span class=\"time\"></span>");
-  var $tweets = $("<ul class=\"tweets\"></ul>");
+  var $tweets = $("<ol class=\"tweets\"></ol>");
 
   var $copy = $("<div class=\"copy\"><h2>Synchronised tweets:</h2></div>")
     .append($time)
@@ -45,11 +55,20 @@
   $("#emp-container").css("overflow", "visible");
 
   if(!authToken()) {
-    var loginURL = "http://iplayer-sync.heroku.com/login?source_url=" + escape(window.location);
+    var loginURL = twitterHost + "/login?source_url=" + escape(window.location);
     $copy.append("<div class=\"auth\"><a href=\"" + loginURL + "\">Log in to Twitter</a></div>");
     return;
   }
 
   var startTime = $(".first_broadcast_date span").text(),
-      duration = $(".duration span").text();
+      duration = parseInt($(".duration span").text(), 10);
+
+  var tweetsURL = twitterHost + "/tweets?remember_token=" + escape(authToken()) + "&start=" + escape(startTime) + "&duration=" + escape(duration);
+  chrome.extension.sendRequest({"action": "loadTweets", "url": tweetsURL}, function(json) {
+    var tweets = JSON.parse(json);
+    for(var i = 0; i < tweets.length; i++) {
+      displayTweet(tweets[i]);
+    }
+  });
+
 })(jQuery);
